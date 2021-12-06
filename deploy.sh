4519d3dd5e78f2da0e0b680fe8ec1baccc1097f8
Daniel Pataki <danielpataki@Daniels-Mac-mini.fritz.box>
2021-12-06 15:28:37 +0100

    Web process

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,2 @@
-git clone https://github.com/WordPress/WordPress.git .
-rm wp-config.php
-mv config.php wp-config.php
+git clone https://github.com/WordPress/WordPress.git ./wordpress
+mv ./wordpress/* ./
